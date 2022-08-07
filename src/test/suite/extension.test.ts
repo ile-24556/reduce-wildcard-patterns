225b6268c12447ef4780c9a3066c7596b7cd42c1
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2022-08-07 21:55:07 +0900

    Change test format

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -2,7 +2,14 @@ import * as assert from 'assert';
 import { makeLinesFight } from '../../reduce-patterns';
 
 suite('Extension Test Suite', () => {
+
+    const knownCases = [
+        { input: ['a', 'bravo', '*', 'charlie'], expected: ['*'] }
+    ];
+
     test('Reduce cases', () => {
-        assert.deepStrictEqual(makeLinesFight(['a', 'bravo', '*', 'charlie']), '*');
+        for (let pair of knownCases) {
+            assert.deepStrictEqual(makeLinesFight(pair.input), pair.expected);
+        }
     });
 });
