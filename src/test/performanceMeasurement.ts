c5a8d230f691a826adf6fed2147e8192fe108a9e
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2022-08-20 21:58:05 +0900

    Add performance measurement script

---
+++ src/test/performanceMeasurement.ts
@@ -0,0 +1,22 @@
+import { performance } from 'perf_hooks';
+import { makeLinesFight } from '../reduce-patterns';
+import * as m365Endpoints from './suite/microsoft365-worldwide.json';
+
+
+export function measure() {
+    const flattenUrls: string[] = [];
+    for (const endpoint of m365Endpoints) {
+        if (!endpoint.urls) {
+            continue;
+        }
+        for (const url of endpoint.urls) {
+            flattenUrls.push(url);
+        }
+    }
+    const startTime = performance.now();
+    for (let i = 0; i < 10000; i++) {
+        makeLinesFight(flattenUrls);
+    }
+    const endTime = performance.now();
+    console.log(endTime - startTime);
+}
